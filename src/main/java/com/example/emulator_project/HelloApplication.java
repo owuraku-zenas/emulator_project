06989b5d967aa49350f3023c1a5d17b467cce5f6
axornam <axornam.richard@gmail.com>
2021-10-19 18:52:56 +0000

    Modify application resolution Add stylesheets

--- src/main/java/com/example/emulator_project/HelloApplication.java
+++ src/main/java/com/example/emulator_project/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 800);
+        Scene scene = new Scene(fxmlLoader.load(), 360, 680);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
