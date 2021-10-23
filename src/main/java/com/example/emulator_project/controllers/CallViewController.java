c0dcf6f7e119eb302dac179d03f55a502d429730
axornam <axornam.richard@gmail.com>
2021-10-23 17:38:32 +0000

    Add callview controller and Updates contactcontroller

--- src/main/java/com/example/emulator_project/controllers/CallViewController.java
+++ src/main/java/com/example/emulator_project/controllers/CallViewController.java
@@ -1,2 +1,14 @@
-package com.example.emulator_project.controllers;public class CallViewController {
+package com.example.emulator_project.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class CallViewController extends NavigationController {
+    @FXML
+    private Label callerID;
+
+    @FXML
+    public void setCallerID(String callerID) {
+        this.callerID.setText(callerID);
+    }
 }
--- src/main/java/com/example/emulator_project/controllers/ContactsController.java
+++ src/main/java/com/example/emulator_project/controllers/ContactsController.java
@@ -1,7 +1,36 @@
 package com.example.emulator_project.controllers;
 
+import com.example.emulator_project.utils.SceneManager;
+import com.example.emulator_project.utils.Views;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class ContactsController extends NavigationController {
 
+    @FXML
+    private TextField dialInputField;
+
+    private SceneManager sm = SceneManager.getInstance();
+
+    public void handleButtonClick(ActionEvent event) {
+        Button button = (Button) event.getTarget();
+        String inputString = dialInputField.getText() + button.getText();
+        if (dialInputField != null) {
+            dialInputField.setText(inputString);
+            System.out.println(inputString);
+        } else {
+            System.out.println(dialInputField.toString() + " is null");
+        }
+    }
+
+
+    public void makePhoneCall(ActionEvent event) {
+        FXMLLoader fx = sm.switchSceneFromEvent(event, Views.CALL_VIEW);
+        ((CallViewController) fx.getController()).setCallerID(dialInputField.getText());
+        sm.load();
+
+    }
 }
