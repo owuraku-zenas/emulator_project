3d2f8e32d2fa09c303461cc6980aa656188192aa
axornam <axornam.richard@gmail.com>
2021-11-03 11:45:50 +0000

    Add click listener to call contacts

--- src/main/java/com/example/emulator_project/controllers/templates/ContactItemController.java
+++ src/main/java/com/example/emulator_project/controllers/templates/ContactItemController.java
@@ -1,12 +1,18 @@
 package com.example.emulator_project.controllers.templates;
 
+import com.example.emulator_project.controllers.CallViewController;
+import com.example.emulator_project.utils.SceneManager;
+import com.example.emulator_project.utils.ViewType;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 public class ContactItemController {
+    private SceneManager sm = SceneManager.getInstance();
+
     @FXML
     private Label contactName;
     @FXML
@@ -30,6 +36,13 @@ public class ContactItemController {
     }
 
     public void handleClick(MouseEvent mouseEvent) {
-        System.out.println("Contact Item:" + contactName.getText());
+        String phoneNumber = contactNumber.getText();
+
+        FXMLLoader fx = sm.setSceneFromView(ViewType.CALL_VIEW);
+        CallViewController cvc = fx.getController();
+        cvc.makePhoneCall(phoneNumber);
+        System.out.println("Calling :" + contactName.getText());
+        sm.loadScene();
+
     }
 }
